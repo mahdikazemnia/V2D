c54e06859979b15204a02d8bc1fd357c970ffaa4
Mahdi Kazemnia <mahdikazemnia@users.noreply.github.com>
2018-01-18 12:13:53 +0330

    initialization

---
+++ V2D.js
@@ -0,0 +1,18 @@
+class V2D {
+
+    constructor() {
+
+        if (arguments.length === 2) { // (0,0)
+            this.x = arguments[0];
+            this.y = arguments[1];
+        } else if (Array.isArray(arguments[0])) { // ([0,0])
+            this.x = arguments[0][0];
+            this.y = arguments[0][1];
+        } else { // ({x:0, y:0})
+            this.x = arguments[0].x;
+            this.y = arguments[0].y;
+        }
+        
+    }
+
+}
